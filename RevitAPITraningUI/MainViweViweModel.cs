ecedb67895944ed7a7fcd16db8d1f5fdbff11a64
Владимир Гаврилов <saxar.1kg@mail.ru>
2023-01-24 21:41:39 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraningUI/MainViweViweModel.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraningUI
+{
+     public class MainViweViweModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SelectCommand { get; }
+
+        public MainViweViweModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SelectCommand = new DelegateCommand(OneSelectCommand);
+        }
+
+        public event EventHandler Closerequest;
+        private void RaiseCloseRequest()
+        {
+            Closerequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OneSelectCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+        }
+    }
+}
